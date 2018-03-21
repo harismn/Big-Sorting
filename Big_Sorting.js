00b16a7b5965eaf1676370d5d6f10149178a0e8e
harismn <harismannug@gmail.com>
2018-03-21 15:06:52 +0700

    first commit

---
+++ Big_Sorting.js
@@ -0,0 +1,48 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function bigSorting(arr) {
+    // Complete this function
+    let berurut = arr.sort((a,b) => {
+        if(a.length == b.length){
+            if(a > b){
+                return 1
+            }else {
+                return -1
+            }
+        }
+        return a.length - b.length;
+    })
+        console.log(berurut.join('\n'))
+    }
+function main() {
+    var n = parseInt(readLine());
+    var arr = [];
+    for(var arr_i = 0; arr_i < n; arr_i++){
+       arr[arr_i] = readLine();
+    }
+    var result = bigSorting(arr);
+    console.log(result.join("\n"));
+
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Big-Sorting
+
+* Problem Name : bigSorting *
+
+* Author: _mfv_*
+
+* url: https://www.hackerrank.com/challenges/big-sorting/problem *
+
+* code *
+unction bigSorting(arr) {
+    // Complete this function
+    let berurut = arr.sort((a,b) => {
+        if(a.length == b.length){
+            if(a > b){
+                return 1
+            }else {
+                return -1
+            }
+        }
+        return a.length - b.length;
+    })
+        console.log(berurut.join('\n'))
+    }
+
+* how to run the program *
+
+$ node big_Sorting.js
